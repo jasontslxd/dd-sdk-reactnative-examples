7daab00bd9c644e1850fdbdb821a3d0e711ee58f
Jason Tsang <ckyjjt@gmail.com>
2024-11-28 13:08:18 +0800

    native api call

--- rn-app-with-native/android/app/src/main/java/com/reactnativewithiosscreen/CustomViewModule.kt
+++ rn-app-with-native/android/app/src/main/java/com/reactnativewithiosscreen/CustomViewModule.kt
@@ -2,6 +2,11 @@ package com.reactnativewithiosscreen
 
 import android.content.Intent
 import com.facebook.react.bridge.*
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.datadog.android.DatadogInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 
 class CustomViewModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "CustomView"
@@ -12,6 +17,34 @@ class CustomViewModule(reactContext: ReactApplicationContext) : ReactContextBase
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         reactApplicationContext.startActivity(intent)
         promise.resolve(true)
+    }
+
+    @ReactMethod
+    fun callApiFromNative(promise: Promise) {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(DatadogInterceptor())
+            .build()
+ 
+        val request = Request.Builder()
+            .url("https://httpbin.org/get?params=native-api-call-from-react-native")
+            .build()
+ 
+        Thread {
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+ 
+                    val responseBody = response.body?.string() ?: "No Response Body"
+                    // Emit event to React Native with the API response
+                    reactApplicationContext
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+                        .emit("nativeApiResponse", responseBody)
+                    promise.resolve("API call successful")
+                }
+            } catch (e: Exception) {
+                promise.reject("API_CALL_FAILED", e)
+            }
+        }.start()
     }
 }
 
--- rn-app-with-native/src/ConfirmationScreen.js
+++ rn-app-with-native/src/ConfirmationScreen.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, Button, NativeModules} from 'react-native';
 
 export const ConfirmationScreen = () => {
+  const [apiResponse, setApiRespnose] = useState('');
+
+  const callNativeApi = () => {
+    NativeModules.CustomView.callApiFromNative()
+      .then((response) => {
+        setApiRespnose(response);
+      })
+      .catch((error) => {
+        setApiRespnose(error);
+      });
+  };
+
   return (
     <>
       <Text>This is a confirmation screen</Text>
+
+      {apiResponse && <Text>api response: {JSON.stringify(apiResponse)}</Text>}
+
       <Button
         title="Start native screen"
         onPress={() => NativeModules.CustomView.startView()}
+      />
+      <Button 
+        title="Call api using native method"
+        onPress={() => callNativeApi()}
       />
     </>
   );
